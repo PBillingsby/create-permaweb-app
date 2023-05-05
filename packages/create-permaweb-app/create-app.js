e29babb6df5db6e16d57d52cfb9d3e6936a91bf6
PBillingsby <philbillingsby@gmail.com>
2023-05-05 11:10:10 -0400

    add arkb install and fund bundlr

--- packages/create-permaweb-app/create-app.js
+++ packages/create-permaweb-app/create-app.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const chalk = require('chalk');
+const { execSync } = require('child_process');
 const fs = require('fs');
 const os = require('os');
 const cpy = require('cpy');
@@ -119,6 +120,7 @@ const init = async ({
   if (bundlr) {
     const packageJsonPath = path.join(root, '/package.json');
     const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+    if (bundlr !== 'no') packageJson.scripts.fundBundlr = `arkb fund-bundlr --use-bundler https://${bundlr}.bundlr.network -w wallet.json $AMOUNT`;
 
     switch (bundlr) {
       case 'no':
