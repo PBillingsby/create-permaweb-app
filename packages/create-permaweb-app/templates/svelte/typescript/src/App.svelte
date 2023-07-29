36480892a2c6ca3e74d12a90fbb2694926bc1aed
PBillingsby <philbillingsby@gmail.com>
2023-07-29 11:39:13 -0400

    update routes for typescript

--- packages/create-permaweb-app/templates/svelte/typescript/src/App.svelte
+++ packages/create-permaweb-app/templates/svelte/typescript/src/App.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-  import { Route, router } from 'tinro';
+  import { Route as TinroRoute, router } from 'tinro';
   import Announcer from './components/announcer.svelte';
   import Transition from './components/transition.svelte';
   import Home from './home.svelte';
   router.mode.hash();
   router.subscribe((_) => window.scrollTo(0, 0));
+  const Route: any = TinroRoute;
 </script>
 
 <main />
